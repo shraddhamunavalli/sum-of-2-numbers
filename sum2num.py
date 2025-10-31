e351caca83de72a89eb1bd5bcb3ade782c7c0e25
shraddha munavalli <munavallishraddha4@gmail.com>
2025-10-31 08:45:54 +0530

    save

---
+++ sum2num.py
@@ -0,0 +1,10 @@
+# Program to find the sum of two numbers
+# Read two numbers from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate sum
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
